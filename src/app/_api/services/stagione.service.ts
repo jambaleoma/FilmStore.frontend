74f262cedb27afadfa84b732330d82250bb260e6
Vincenzo <vincenzodamico93@gmail.com>
2018-10-29 04:58:41 +0100

    add new crud operation for stagione

--- src/app/_api/services/stagione.service.ts
+++ src/app/_api/services/stagione.service.ts
@@ -6,7 +6,6 @@ import { ApiConfiguration } from '../api-configuration';
 import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators/map';
 import { filter } from 'rxjs/operators/filter';
-import { Serie } from '../models';
 import { Stagione } from '../models/stagione';
 
 @Injectable()
@@ -133,5 +132,135 @@ export class StagioneService extends BaseService {
       map(_r => _r.body)
     );
   }
+
+  /**
+     *
+     * - `id`: 
+     *
+     * - `body`: 
+     *
+     * @return Updated Stagione
+     */
+    private updateStagioneResponse(params: Stagione): Observable<HttpResponse<Stagione[]>> {
+      let __params = this.newParams();
+      let __headers = new HttpHeaders();
+      let __body: any = null;
+
+      __body = params;
+      let req = new HttpRequest<any>(
+          "PUT",
+          this.rootUrl + 'rest/stagioni/upDateStagioneById/' + params.stagione_id,
+          __body,
+          {
+              headers: __headers,
+              params: __params,
+              responseType: 'json'
+          });
+
+      return this.http.request<any>(req).pipe(
+          filter(_r => _r instanceof HttpResponse),
+          map(_r => {
+              let _resp = _r as HttpResponse<any>;
+              let _body: Stagione[] = null;
+              _body = _resp.body as Stagione[];
+              return _resp.clone({ body: _body }) as HttpResponse<Stagione[]>;
+          })
+      );
+  }
+
+  /**
+   *
+   * - `id`: 
+   *
+   * - `body`: 
+   *
+   * @return Updated Stagione
+   */
+  updateStagione(params: Stagione): Observable<Stagione[]> {
+      return this.updateStagioneResponse(params).pipe(
+          map(_r => _r.body)
+      );
+  }
+
+
+  /**
+   * @param id undefined
+   * @return Deleted status
+   */
+  private deleteStagioneResponse(id: string): Observable<HttpResponse<boolean>> {
+      let __params = this.newParams();
+      let __headers = new HttpHeaders();
+      let __body: any = null;
+
+      let req = new HttpRequest<any>(
+          "DELETE",
+          this.rootUrl + 'rest/stagioni/deleteStagioneById/' + id,
+          __body,
+          {
+              headers: __headers,
+              params: __params,
+              responseType: 'text'
+          });
+
+      return this.http.request<any>(req).pipe(
+          filter(_r => _r instanceof HttpResponse),
+          map(_r => {
+              let _resp = _r as HttpResponse<any>;
+              let _body: boolean = null;
+              _body = _resp.body == 'true';
+              return _resp.clone({ body: _body }) as HttpResponse<boolean>;
+          })
+      );
+  }
+
+  /**
+   * @param id undefined
+   * @return Deleted status
+   */
+  deleteStagione(id: string): Observable<boolean> {
+      return this.deleteStagioneResponse(id).pipe(
+          map(_r => _r.body)
+      );
+  }
+
+    /**
+   * @param id undefined
+   * @return Deleted status
+   */
+  private deleteStagioniBySerieIdResponse(id: string): Observable<HttpResponse<boolean>> {
+    let __params = this.newParams();
+    let __headers = new HttpHeaders();
+    let __body: any = null;
+
+    let req = new HttpRequest<any>(
+        "DELETE",
+        this.rootUrl + 'rest/stagioni/deleteStagioniBySerieId/' + id,
+        __body,
+        {
+            headers: __headers,
+            params: __params,
+            responseType: 'text'
+        });
+
+    return this.http.request<any>(req).pipe(
+        filter(_r => _r instanceof HttpResponse),
+        map(_r => {
+            let _resp = _r as HttpResponse<any>;
+            let _body: boolean = null;
+            _body = _resp.body == 'true';
+            return _resp.clone({ body: _body }) as HttpResponse<boolean>;
+        })
+    );
+}
+
+/**
+ * @param id undefined
+ * @return Deleted status
+ */
+deleteStagioniBySerieId(id: string): Observable<boolean> {
+    return this.deleteStagioniBySerieIdResponse(id).pipe(
+        map(_r => _r.body)
+    );
+}
 
 }
